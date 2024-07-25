e4942deaa5b3506f07e780865e990cccbd06ed3c
landing page <talhadeveloper.1@gmail.com>
2024-07-25 21:45:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//console.log("Hello World");
+// var a = [12]
+// var b = a;
+// alert("Hello")
+// alert("Im")
+// alert("Your")
+// alert("Website")
+// alert("life")
+
+// var a = "Talha";
+// for(var i = 0; i<11; i++){
+//     console.log(i)
+// // }
+// var a = "How are you";
+// a = 4;
+// alert(a)
+
+// var num = 1;
+//  num = 2;
+// console.log(num);
+var anas = 5*3;
+var zakeer =anas+5;
+console.log(zakeer );
